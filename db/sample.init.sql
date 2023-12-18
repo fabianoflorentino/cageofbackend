ff19b41b36940960a17397c35d2191340bf52ec3
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2023-12-18 19:27:36 -0300

    chore: sample sql

---
+++ db/sample.init.sql
@@ -0,0 +1,16 @@
+SET GLOBAL host_cache_size=0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+SET NAMES utf8mb4;
+
+CREATE DATABASE IF NOT EXISTS `cageofbackend_development`;
+CREATE DATABASE IF NOT EXISTS `cageofbackend_test`;
+CREATE DATABASE IF NOT EXISTS `cageofbackend`;
+
+CREATE USER IF NOT EXISTS 'cage'@'%' IDENTIFIED BY "<PASSWORD>";
+CREATE USER IF NOT EXISTS 'cage'@'localhost' IDENTIFIED BY "<PASSWORD>";
+
+GRANT ALL PRIVILEGES ON *.* TO 'cage'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'cage'@'localhost';
